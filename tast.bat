13358c6939ca85156faf5f68bebc0093b9f13725
TEFSOM <97161031+TEFSOM@users.noreply.github.com>
2022-01-05 21:07:27 +0900

    Update tast.bat

--- tast.bat
+++ tast.bat
@@ -1,5 +1,6 @@
-::中心x,中心y,半径,線幅,変調率,極対数,データ数,本数,細線幅
-::center position x, center position y, radius, pitch of concentric circles, modulation ratio, number of pole pairs, number of data, number of concentric circles, width of line
+rem 中心x,中心y,半径,線幅,変調率,極対数,データ数,本数,細線幅
+rem center position x, center position y, radius, pitch of concentric circles, modulation ratio, number of pole pairs, number of data, number of concentric circles, width of line
+rem delete these comments if this is not work well due to character code or something
 test.exe 50 50 20 1 0.999 7 250 4 0.08
 del test1.txt
 ren test.txt test1.txt
